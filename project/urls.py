870896b7ee3e530a2ab389f091f02f4ccf5f03ec
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-07-24 15:48:15 +0530

    url change

--- project/urls.py
+++ project/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT})
 ]
 
-if settings.DEBUG:
-  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
