4e3d68aae787052fa15b9d2ddf032d4fadbc297f
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-07-24 09:59:00 +0530

    fixed media url not found bug in heroku app

--- project/urls.py
+++ project/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("product_categories.urls")),
     path("", include("product_items.urls")),
     path("", include("accounts.urls")),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT})
 ]
 
 if settings.DEBUG:
