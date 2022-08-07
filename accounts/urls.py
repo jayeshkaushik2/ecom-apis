79aaf11c30f88c0529b8ea3c490a3f6e9743aecf
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-08-07 10:16:25 +0530

    fixed url for user-profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('create-user-profile/', views.CreateUserApi, name='create-user-profile'),
+    path('user-profile/', views.CreateUserApi, name='user-profile'),
 ]
