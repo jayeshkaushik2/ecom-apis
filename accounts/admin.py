5bba389df2f99248e2cf0e5d9baefb9b72f19294
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-09-06 22:12:24 +0530

    added models to admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Address
+from .models import Profile, Address, UserEmail, UserPhone
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Address)
+admin.site.register(UserEmail)
+admin.site.register(UserPhone)
--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Homepage, Detail, Homepage_Images, Founder
+from .models import CompanyEmail, Homepage, Detail, Homepage_Images, Founder
 
 # Register your models here.
 admin.site.register(Homepage_Images)
 admin.site.register(Founder)
 admin.site.register(Detail)
 admin.site.register(Homepage)
+admin.site.register(CompanyEmail)
