cafc6afeecce24021447458a29827e358f670510
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2023-03-22 10:12:43 +0530

    fix save method in sub category

--- product_categories/models.py
+++ product_categories/models.py
@@ -22,8 +22,8 @@ class Sub_category(models.Model):
     def __str__(self) -> str:
         return self.name
 
-    def save(self) -> None:
+    def save(self, *args, **kwargs) -> None:
         if self.thumb is None or self.thumb == "":
             image_to_thumb(obj=self, image_name="image")
             self.updated = True
-        return super().save()
+        return super().save(*args, **kwargs)
