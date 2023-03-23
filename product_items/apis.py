848c78839d461738cedfa65034733b56bd3404dc
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2023-03-23 20:21:35 +0530

    fix: added custom field.

--- product_items/apis.py
+++ product_items/apis.py
@@ -3,6 +3,7 @@ from .models import ProductImage, Product
 from .serializers import Product_ImagesSz, ProductSz
 from django.conf import settings
 from django_filters import rest_framework as filters
+from django.db.models import Q
 
 
 class Product_ImageApi(viewsets.ModelViewSet):
@@ -13,12 +14,20 @@ class Product_ImageApi(viewsets.ModelViewSet):
 
 
 class ProductFilter(filters.FilterSet):
+    sub_category_or_title = filters.CharFilter(method="get_sub_category_or_title")
+
+    def get_sub_category_or_title(self, queryset, value, *args, **kwargs):
+        val = args[0]
+        return queryset.filter(
+            Q(sub_category__name__icontains=val) | Q(title__icontains=val)
+        )
+
     class Meta:
         model = Product
         fields = {
             "sub_category__name": ("exact", "icontains"),
             "is_promoted": ("exact",),
-            "title": ("exact",),
+            "title": ("exact", "icontains"),
             "price": ("lt", "gt", "lte", "gte"),
             "rating": ("lt", "gt", "lte", "gte"),
             "discount_pct": ("lt", "gt", "lte", "gte"),
