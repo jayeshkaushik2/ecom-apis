d5570f5d99de44edbcf11dd4a7f5623e945d4833
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2023-03-23 16:19:20 +0530

    feature: image to thumb update.

--- common/ImageThumbCovert.py
+++ common/ImageThumbCovert.py
@@ -4,12 +4,17 @@ from django.core.files.base import ContentFile
 
 
 def image_to_thumb(obj, image_name, thumbSize=[300, 320]):
-    image = getattr(obj, image_name)
-    name, typ = image.name.split(".")
-    img = Image.open(image)
-    img.thumbnail(thumbSize)
-    img.save("te.jpg")
-    thumb = io.BytesIO()
-    obj.thumb.save(f"thumb-{obj.id}.{typ}", thumb)
+    image_file = getattr(obj, image_name)
+    if image_file.name == "":
+        return
+    name, typ = image_file.name.split(".")
+    image = Image.open(image_file)
+    image.thumbnail(thumbSize)
+    # img.save("te.jpg")
+    dst_bytes = io.BytesIO()
+    image.save(dst_bytes, typ)
+    dst_bytes.seek(0)
+    obj.thumb.save(name, ContentFile(dst_bytes.read()), save=False)
+    dst_bytes.close()
     obj.save()
     return None
